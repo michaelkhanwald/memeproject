af69211cb1b906d2232c51679fb4def695e04e25
michaelkhanwald <noor.sadaf29@gmail.com>
2021-02-11 00:05:29 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod.service
+sudo systemctl enable mongod
+
+
