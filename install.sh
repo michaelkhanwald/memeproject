a90cca0aba02754d174b264c1bb409538135e7ac
michaelkhanwald <noor.sadaf29@gmail.com>
2021-02-11 04:44:28 +0530

    script change

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-node -v
-npm -v
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+
 
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
