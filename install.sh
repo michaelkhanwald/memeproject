0708bc78bcf07f835f4857622e658015c8393278
michaelkhanwald <noor.sadaf29@gmail.com>
2021-02-11 02:11:18 +0530

    script changes

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo yum install -y nodejs
 
-[mongodb-org-4.4]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.4/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
-
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo yum add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/yum/sources.list.d/mongodb-org-4.2.list
+sudo yum update
 sudo yum install -y mongodb-org
-sudo yum install -y mongodb-org-4.4.3 mongodb-org-server-4.4.3 mongodb-org-shell-4.4.3 mongodb-org-mongos-4.4.3 mongodb-org-tools-4.4.3
+sudo systemctl start mongod
+sudo systemctl status mongod
+sudo systemctl enable mongod
+
+
+
 
 
 
