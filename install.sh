b6d929a8bd12e4ffadea5636127a32fe71e35196
michaelkhanwald <noor.sadaf29@gmail.com>
2021-02-11 01:53:15 +0530

    script change

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo yum install -y nodejs
 
-sudo vi /etc/yum.repos.d/mongodb-org.repo
-[mongodb-org-3.4]
+[mongodb-org-4.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.4/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
 
 sudo yum install -y mongodb-org
-sudo systemctl start mongod
+sudo yum install -y mongodb-org-4.4.3 mongodb-org-server-4.4.3 mongodb-org-shell-4.4.3 mongodb-org-mongos-4.4.3 mongodb-org-tools-4.4.3
 
 
 
