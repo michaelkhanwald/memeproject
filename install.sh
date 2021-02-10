96aa603d88d1e993d786f158807a1c3c92eb81e2
michaelkhanwald <noor.sadaf29@gmail.com>
2021-02-11 01:38:31 +0530

    script change

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo yum install -y nodejs
 
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo yum-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo yum update
-sudo  yum install -y mongodb-org
-sudo systemctl start mongod.service
-sudo systemctl enable mongod
+sudo vi /etc/yum.repos.d/mongodb-org.repo
+[mongodb-org-3.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+
+sudo yum install -y mongodb-org
+sudo systemctl start mongod
+
 
 
